7ac285027c64c7df2c8dd8103f99b29f73664b15
jacobjmarks <jacobjmarks@outlook.com>
2018-05-03 12:14:32 +1000

    Setup basic routes and views

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
+const regprecise = require("./libs/regprecise-rest.js");
+
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-    res.end("Hello, World!");
+    regprecise.getGenomes((err, genomes) => {
+        if (err) return res.status(500).end(err);
+        res.render("genomes.pug", { genomes: genomes });
+    })
+});
+
+app.get("/:genomeId", (req, res) => {
+    res.end();
 });
 
 app.listen(PORT, () => {
